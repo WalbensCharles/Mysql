e10517a978cc0e04b79c157ee3c7a5e90e2b2f9f
Walbens Charles <walbensbenbens3@gmail.com>
2024-12-16 15:53:18 -0300

    Fdatabese

---
+++ estuda_mysql.session.sql
@@ -0,0 +1,19 @@
+create database cadastro;
+
+use cadastro;
+
+create table pessoas(
+nome varchar(30),
+sexo char(1),
+idade tinyint,
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+
+show tables;
+describe pessoas;
+
+
+
