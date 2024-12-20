50ec8ead42e99a91ad1d30ad3e359edd330da4ce
Walbens Charles <walbensbenbens3@gmail.com>
2024-12-20 08:26:23 -0300

    s

--- estuda_mysql.session.sql
+++ estuda_mysql.session.sql
@@ -11,9 +11,74 @@ altura float,
 nacionalidade varchar(20)
 );
 
-
+use cadastro2;
 show tables;
 describe pessoas;
 
-drop database cadastro;
+alter table pessoas
+add column profissao varchar(10);
+select * from pessoas;
 
+alter table pessoas
+drop column profissao;
+
+alter table pessoas
+add column profissao varchar(10) after nome;
+
+alter table pessoas 
+add column codigo int first;
+
+alter table pessoas 
+modify column profissao varchar(20) not null default '';
+
+
+alter table pessoas 
+change column profissao prof varchar(20);
+
+alter table pessoas
+rename to gafanhotos;
+
+desc gafanhotos;
+
+
+create table if not exists cusros(
+    nome varchar(30) not null unique,
+    descricaos text,
+    carga int unsigned,
+    totaulas text,
+    ano year default '2016'
+
+)default charset=utf8;
+
+alter table cusros
+rename to cursos;
+
+
+use cadastro2;
+
+desc cursos ;
+
+alter table cursos 
+add column idcurso int first;
+
+alter table cursos 
+add primary key(idcurso);
+
+create table if not exists teste(
+    id int,
+    nome varchar(10),
+    idade int
+);
+
+insert into teste VALUES
+('1','naria','18'),
+('78','jose','45'),
+('87','pierre','88');
+
+desc teste;
+
+alter table teste
+
+select * from teste;
+
+drop table if exists teste;
